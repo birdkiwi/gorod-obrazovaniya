84f7a5c68ba1d8b707e87b456a98854c02676797
Alexey Toklovich <alexey@toklovich.com>
2017-07-05 13:51:12 +0700

    Login modal

---
+++ blocks/modal-login.php
@@ -0,0 +1,23 @@
+<div class="login-modal-title">
+    Войти на сайт
+</div>
+
+<form action="#" method="POST" class="login-modal-form" data-validate>
+    <div class="form-group">
+        <label for="login-modal-login" class="form-label">Логин</label>
+        <input id="login-modal-login" type="text" class="form-input" required>
+    </div>
+
+    <div class="form-group">
+        <label for="login-modal-password" class="form-label">Пароль</label>
+        <input id="login-modal-password" type="password" class="form-input" minlength="6" required>
+    </div>
+
+    <div class="form-group">
+        <button type="submit" class="button button-red button-block">Войти</button>
+    </div>
+
+    <div class="login-modal-links m-t-lg m-b-sm text-center">
+        <a href="#">Забыли пароль?</a>
+    </div>
+</form>
