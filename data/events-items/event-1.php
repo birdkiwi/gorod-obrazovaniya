2b8f380c6fb4bcd59b7875f414c8cc1b8ed6355d
Alexey Toklovich <alexey@toklovich.com>
2017-08-14 15:19:35 +0700

    Event modal

--- data/events-items/event-1.php
+++ data/events-items/event-1.php
@@ -1,20 +1,23 @@
 <div class="side-modal-event-title">
     Дискуссии о ключевых трендах развития образования
 </div>
-<div class="side-modal-event-meta">
-    <div class="side-modal-event-meta-date">
-        <span class="side-modal-event-meta-date-icon">
-            <?php include "../../images/icons/icon-modal-calendar.svg"; ?>
-        </span>
-        7 сентября, 14:00
-    </div>
-    <div class="side-modal-event-meta-place">
-        <span class="side-modal-event-meta-place-icon">
-            <?php include "../../images/icons/icon-modal-marker.svg"; ?>
-        </span>
-        Основной зал 1
-    </div>
+
+<div class="news-item-meta-2 m-b-lg">
+    <span class="news-item-meta-2-date">
+        <?php include "../../images/icons/icon-news-item-clock.svg"; ?>
+        16 июня, 12:30 – 13:30
+    </span>
+    <span class="news-item-meta-2-place">
+        <?php include "../../images/icons/icon-map-mark.svg"; ?>
+        Зал №1
+    </span>
+    <a href="#" class="news-item-meta-2-fav">
+        <?php include "../../images/icons/icon-news-item-fav.svg"; ?>
+        <!--<?php include "../../images/icons/icon-news-item-fav-active.svg"; ?>-->
+        Добавить в избранное
+    </a>
 </div>
+
 <div class="side-modal-event-description">
     <p>
         С 7 по 9 сентября в 75-м павильоне ВДНХ пройдет Московский международный форум «Город образования». На три дня мероприятие представит на одной площадке все образовательные возможности столицы. Организаторы форума ожидают участников со всего мира, в числе спикеров заявлены знаменитый индийский ученый и профессор Ньюкаслского университета Сугата Митра и авторитетный эксперт в области образования Уильям Ранкин.
